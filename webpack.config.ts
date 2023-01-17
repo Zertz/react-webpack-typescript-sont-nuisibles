858924e6787b55498976003d86f7d0ad1018e61e
Pier-Luc Gendreau <pierluc@outlook.com>
2023-01-16 21:25:29 -0500

    Export multiple configurations

--- webpack.config.ts
+++ webpack.config.ts
@@ -10,79 +10,89 @@ const devServer: DevServerConfiguration = {
   static: "./dist",
 };
 
-const cmtsxLoaders: (string | RuleSetUseItem[])[] = [
-  "babel-loader",
-  [
+function getBaseConfiguration(cmtsxLoader: RuleSetUseItem[]): Configuration {
+  return {
+    // @ts-expect-error 🙈
+    name: cmtsxLoader[0].loader.split("-")[0],
+    devtool: "eval-source-map",
+    devServer,
+    entry: "./src/main.tsx",
+    experiments: {
+      lazyCompilation: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /.([cm]?ts|tsx)$/,
+          use: cmtsxLoader,
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader", "postcss-loader"],
+        },
+        {
+          test: /\.png$/i,
+          type: "asset",
+        },
+        {
+          test: /\.txt$/i,
+          type: "asset/source",
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+      extensionAlias: {
+        ".ts": [".js", ".ts"],
+        ".cts": [".cjs", ".cts"],
+        ".mts": [".mjs", ".mts"],
+      },
+    },
+    optimization: {
+      runtimeChunk: "single",
+      splitChunks: {
+        cacheGroups: {
+          react: {
+            test: /[\\/]node_modules[\\/](react|react-dom|react-router-dom)[\\/]/,
+            chunks: "all",
+          },
+        },
+      },
+    },
+    output: {
+      filename: "[name].bundle.js",
+      path: resolve(__dirname, "dist"),
+      clean: true,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "index.html",
+      }),
+      new ReactRefreshWebpackPlugin(),
+    ],
+  };
+}
+
+const configurations = [
+  getBaseConfiguration([
+    {
+      loader: "babel-loader",
+    },
+  ]),
+  getBaseConfiguration([
     {
       loader: "ts-loader",
       options: {
         transpileOnly: true,
       },
     },
-  ],
-  "swc-loader",
+  ]),
+  getBaseConfiguration([
+    {
+      loader: "swc-loader",
+    },
+  ]),
 ];
 
-const cmtsxLoaderIndex = 2;
-
-const config: Configuration = {
-  devtool: "eval-source-map",
-  devServer,
-  entry: "./src/main.tsx",
-  experiments: {
-    lazyCompilation: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /.([cm]?ts|tsx)$/,
-        use: cmtsxLoaders[cmtsxLoaderIndex],
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader", "postcss-loader"],
-      },
-      {
-        test: /\.png$/i,
-        type: "asset",
-      },
-      {
-        test: /\.txt$/i,
-        type: "asset/source",
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-    extensionAlias: {
-      ".ts": [".js", ".ts"],
-      ".cts": [".cjs", ".cts"],
-      ".mts": [".mjs", ".mts"],
-    },
-  },
-  optimization: {
-    runtimeChunk: "single",
-    splitChunks: {
-      cacheGroups: {
-        react: {
-          test: /[\\/]node_modules[\\/](react|react-dom|react-router-dom)[\\/]/,
-          chunks: "all",
-        },
-      },
-    },
-  },
-  output: {
-    filename: "[name].bundle.js",
-    path: resolve(__dirname, "dist", `${cmtsxLoaderIndex}`),
-    clean: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "index.html",
-    }),
-    new ReactRefreshWebpackPlugin(),
-  ],
-};
-
-export default config;
+export default configurations;
